13cd5c685ae406e34d0682a652f35c7f87b3410f
chh20121122 <1806156299@qq.com>
2019-06-30 14:04:45 +0800

    asfa

---
+++ src/com/itheima/aaa.java
@@ -0,0 +1,9 @@
+package com.itheima;
+
+public class aaa {
+    public static void main(String[] args) {
+        System.out.println("lfas");
+        System.out.println("lfas");
+        System.out.println("lfas");
+    }
+}
